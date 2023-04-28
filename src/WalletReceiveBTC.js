68f754039e6c913e8db6f27aa90dd65ce9a4c7e6
B-Perederei <woncaru@gmail.com>
2023-04-28 14:11:56 +0300

    Change receiveBTC

--- src/WalletReceiveBTC.js
+++ src/WalletReceiveBTC.js
@@ -10,32 +10,38 @@ import WalletFooter from "./WalletFooter";
 
 function WalletReceiveBTC() {
   const [address, setAddress] = useState('3Kw2TQ93iMUYCQQ1NcaNkoo3TfaJ3QsZKS');
+  const [copied, setCopied] = useState(false);
 
   const generateAddress = () => {
     // logic to generate a new address
     const newAddress = '3Kw2TQ93iMUYCQQ1NcaNkoo3TfaJ3QsZKS';
     setAddress(newAddress);
+    setCopied(false);
+  }
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(address);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 5000);
   }
 
   return (
     <div>
         <WalletHeader />
         <main id = 'receiveBTC'>
-                <div class = "qrcode-container" id = "receiveBS">
-                    <QRCode value={`bitcoin:${address}`} size={250} />
-                </div>
-                <span>
-                    <button>Copy</button>
-                    <p>3Kw2TQ93iMUYCQQ1NcaNkoo3TfaJ3QsZKS</p>
-                </span>
-                <div>
-                    <button onClick={generateAddress}>Generate another address</button>
-                    <a href="signedup.html">
-                        <button>Return back</button>
-                    </a>
-                </div>  
+          <div class = "qrcode-container" id = "receiveBS">
+              <QRCode value={`bitcoin:${address}`} size={265} />
+          </div>
+          <div class="address-container">
+              <p id="addressText" onClick={copyToClipboard}>{address}</p>
+              {copied && <p id="copiedAddress">Copied!</p>}
+            </div>
+          
+          <button onClick={generateAddress} id="generateAddressButton">Generate another address</button>
+          <a href="/wallet" class="buttonForwarding">
+              Return back
+          </a>    
         </main>
-
         <WalletFooter />
     </div>
   );
